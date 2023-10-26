2516460610773cab19191406f47ec99cb58ba07f
feryndka <feryandika151@gmail.com>
2023-10-26 19:47:42 +0700

    fix(exercise_3) : fix number 11

--- modul-1/exercise_3/11-dataType.js
+++ modul-1/exercise_3/11-dataType.js
@@ -1,10 +1,8 @@
 let input = "hello"
-let str = ""
-let n = 1
 
-if (typeof (input) === typeof (str)) {
+if (typeof (input) === "string") {
     console.log(1);
-} else if (typeof (input) == typeof (n)) {
+} else if (typeof (input) == "number") {
     console.log(2);
 } else {
     console.log(3);
